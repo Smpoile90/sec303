94562b3bd0a4e2bc21393a1e3d999ca7567d4320
Simon <simon.m.poile@gmail.com>
2019-01-08 16:22:22 +0000

    Hmm

--- intelliScan.py
+++ intelliScan.py
@@ -3,17 +3,21 @@ import scanner
 import dbhandler
 import json
 import vuln
-from TIfeeds import vulDB,vulnershdlr
+from TIfeeds import vulDB, vulnershdlr
 
-#Parse args using argparse
+
+# Parse args using argparse
 def parse_args():
     parser = argparse.ArgumentParser(description="Intelliscan threat intelligence integrated vulnerability scanner")
     parser.add_argument('host', help='The hostname, IP address or range to scan')
-    parser.add_argument('-p',dest='ports', help='The port (comma separated list) or range of ports to be scanned',nargs='?')
-    parser.add_argument('-d', dest='dumb', help='Disable threat intelligence',action='store_true')
-    parser.add_argument('-fA', dest='forceALL', help="Force use of all threat intelligence sources",action='store_true')
-    parser.add_argument('-oJ',dest="FILE", help="Output results in JSON to FILE")
+    parser.add_argument('-p', dest='ports', help='The port (comma separated list) or range of ports to be scanned',
+                        nargs='?')
+    parser.add_argument('-d', dest='dumb', help='Disable threat intelligence', action='store_true')
+    parser.add_argument('-fA', dest='forceALL', help="Force use of all threat intelligence sources",
+                        action='store_true')
+    parser.add_argument('-oJ', dest="FILE", help="Output results in JSON to FILE")
     return parser.parse_args()
+
 
 def queryTI(query):
     x = vulnershdlr.search(query)
@@ -26,54 +30,59 @@ def queryTI(query):
             svvulnList.append(vul)
     return svvulnList
 
+
 def outputResults(file, data):
-    with open(file,'w') as f:
-        json.dump(data,f)
+    with open(file, 'w') as f:
+        json.dump(data, f)
+
 
 def customTest():
-    #TODO
+    # TODO
     pass
 
 
 def main():
-    #Parse arguments
+    # Parse arguments
     args = parse_args()
 
-    ####DELETE AFTER USE
-    #Conduct Scan
+    # Conduct Scan
     if (args.host is not None):
         if args.ports is not None:
-            scan = scanner.scan(args.host,args.ports)
+            scan = scanner.scan(args.host, args.ports)
         else:
             scan = scanner.scan(args.host)
     else:
-        print ("Please enter a valid IP")
+        print("Please enter a valid IP")
         return
 
     ##Begin searching for vulnerabilities
-    #Search caches DB
+    # Search caches DB
     scan = scan['scan']
 
-    #Resultlist contains a dict of lists, port number+ service is the key and list of vulns is the value
-    tests = []
+    # ResultDict contains a dict of lists, port number+ service is the key and list of vulns is the value
     resultDict = {}
-    for hkey,host in scan.items():
-        for pkey,port in host['tcp'].items():
-            svname = port['product']+ " " + port["version"]
+    for hkey, host in scan.items():
+        for pkey, port in host['tcp'].items():
+            svname = port['product'] + " " + port["version"]
             if (args.dumb is True):
-                #Dont query TI
-                #TODO
+                # Dont query TI
+                # TODO
                 pass
             else:
-                #Do query TI
-                resultDict[str(hkey) + "_" +str(pkey)+"_"+svname] = queryTI(svname)
-                #Do query explicit tests
-                tests.append(dbhandler.getTests(str(pkey),port['name']))
-
+                # Do query TI
+                id = str(hkey) + "_" + str(pkey) + "_" + svname
+                resultDict[id] = queryTI(svname)
+                # Do query explicit tests
+                tests = dbhandler.getTests(str(pkey), port['name'])
+                for t in tests:
+                    d = {}
+                    exec(t[3])
+                    test = locals()['test']
+                    test(hkey, pkey)
 
     ##Clean results wihtout vulnerabilties
     dlist = []
-    for k,v in resultDict.items():
+    for k, v in resultDict.items():
         if len(v) == 0:
             dlist.append(k)
 
@@ -81,17 +90,17 @@ def main():
         del resultDict[key]
 
     if args.FILE is not None:
-        for k,v in resultDict.items():
+        for k, v in resultDict.items():
             for vuln in v:
                 resultDict[k] = str(vuln)
 
-        outputResults(args.FILE,resultDict)
+        outputResults(args.FILE, resultDict)
     else:
-        #output to stdout
-        for k,v in resultDict.items():
-            print ("Host_Port_Service: "+ k )
+        # output to stdout
+        for k, v in resultDict.items():
+            print("Host_Port_Service: " + k)
             for vuln in v:
-                print (str(vuln)+ "\n")
+                print(str(vuln) + "\n")
 
 
 if __name__ == "__main__":
